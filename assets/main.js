1c79ca18a09762c70788b73b91598fbd54992402
JR <jfrazier@tiggee.com>
2018-10-18 07:50:31 -0400

    working on adding IP addres

--- assets/main.js
+++ assets/main.js
@@ -4,22 +4,30 @@ const allowMXServers = document.getElementById("allowMXServers");
 const disallowMXServers = document.getElementById("disallowMXServers");
 const form = document.getElementsByClassName("form__spf-wizard");
 
+//Add IP Address
 $(document).ready(() => {
-    $('#relayIPAddress').keyup(() => {
-        console.log("WTF")
-    });
-})
+  let foo = $("#answer").html();
+  const main = ["@ IN TXT", ' "v=spf1', '"'];
+  $("#relayIPAddress").keypress(x => {
+    main.splice(2, 0, "test");
+    console.log(x.originalEvent.key);
+    /*$("#answer").text(x.originalEvent.key);*/
+    console.log(main);
+    $("#answer").text(main.join(" "));
+  });
+  $("#answer").text(main.join(" "));
+});
 
 function getMXServersValue() {
-    if ((allowMXServers.checked = true)) {
-        // return allowMXServers.value;
-        console.log("it worked");
-        // else if ((disallowMXServers.checked = true)) {
-        //     return disallowMXServers.value;
-        // }
-    }
+  if ((allowMXServers.checked = true)) {
+    // return allowMXServers.value;
+    console.log("it worked");
+    // else if ((disallowMXServers.checked = true)) {
+    //     return disallowMXServers.value;
+    // }
+  }
 }
 
 function submitForm() {
-    answer.innerHTML = domain.value + "." + " " + "IN TXT";
+  answer.innerHTML = domain.value + "." + " " + "IN TXT";
 }
