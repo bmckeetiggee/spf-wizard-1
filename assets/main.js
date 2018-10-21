de094b13f2558a0a77e22865b895641d3ccc44fd
JR <jfrazier@tiggee.com>
2018-10-20 21:14:34 -0400

    added auto fill host name for #domainName

--- assets/main.js
+++ assets/main.js
@@ -6,16 +6,29 @@ const form = document.getElementsByClassName("form__spf-wizard");
 
 //Add IP Address
 $(document).ready(() => {
-  let foo = $("#answer").html();
+  //Auto insert domain name into #domainName
+  let domainName = "your domain,";
+  $("#domain").keyup(x => {
+    if (x.originalEvent.key === "Backspace" && domainName.length === 1) {
+      domainName = "your domain,";
+    } else {
+      domainName = $("#domain").val();
+    }
+    $("#domainName").text(domainName);
+  });
+
+  $("#domainName").text(domainName);
+
+  /*let foo = $("#answer").html();
   const main = ["@ IN TXT", ' "v=spf1', '"'];
-  $("#relayIPAddress").keypress(x => {
+    $("#relayIPAddress").keypress(x => {
     main.splice(2, 0, "test");
     console.log(x.originalEvent.key);
-    /*$("#answer").text(x.originalEvent.key);*/
+    /*$("#answer").text(x.originalEvent.key);
     console.log(main);
     $("#answer").text(main.join(" "));
   });
-  $("#answer").text(main.join(" "));
+  $("#answer").text(main.join(" "));*/
 });
 
 function getMXServersValue() {
