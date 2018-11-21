76387c84eada8d069d34ff5217779f34ea5ec559
Blair McKee <bmckee@tiggee.com>
2018-11-21 14:03:09 -0500

    cleaned up and reorganized scripts. started work on relay ip's

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 const answer = document.getElementById("answer");
 const domain = document.getElementById("domain");
 const form = document.getElementsByClassName("form__spf-wizard");
@@ -7,6 +9,7 @@ $(document).ready(() => {
     //Auto insert domain name into #domainName
     let domainNamePlaceHolder = "your domain,";
     let domainName = "";
+
     $("#domain").keyup(x => {
         if (x.originalEvent.key === "Backspace" && $("#domain").val() === "") {
             $("#domainName").text(domainNamePlaceHolder);
@@ -15,10 +18,12 @@ $(document).ready(() => {
             $("#domainName").text(domainName);
         }
     });
+});
 
-    // $("#domainName").text(domainNamePlaceHolder); //is this necessary?
 
-    /*let foo = $("#answer").html();
+// $("#domainName").text(domainNamePlaceHolder); //is this necessary?
+
+/*let foo = $("#answer").html();
   const main = ["@ IN TXT", ' "v=spf1', '"'];
     $("#relayIPAddress").keypress(x => {
     main.splice(2, 0, "test");
@@ -29,96 +34,76 @@ $(document).ready(() => {
   });
   $("#answer").text(main.join(" "));*/
 
-    //auto insert domain name in #answer
-    let yourDomain = "example.com";
-    let recordInfo = ". IN TXT";
-    $("#domain").keyup(x => {
-        if (x.originalEvent.key === "Backspace" && yourDomain.length === 1) {
-            yourDomain = "example.com";
-        } else {
-            yourDomain = $("#domain").val();
-        }
-        $("#answer")
-            .text(yourDomain)
-            .append(recordInfo);
-        //i need this to also run the getAnswers function when yourDomain value changes
-    });
-
-    //    $("#answer").text(yourDomain);
-
-    //MX Servers, IP Address, Hostnames, and Strict inputs
-    //reruns on every input click no long duplicates answers in answer box
-
-    $(
-        "input[name=MXServers], input[name=IPAddress], input[name=hostnames], input[name=strict]"
-    ).on("click", function getAnswers() {
-        let MXServers = $("input[name=MXServers]:checked").val();
-        let IPAddress = $("input[name=IPAddress]:checked").val();
-        let Hostnames = $("input[name=hostnames]:checked").val();
-        let strict = $("input[name=strict]:checked").val();
-
-        // $(printToAnswer);
-
-        $("#answer")
-            .text(yourDomain)
-            .append(
-                recordInfo,
-                " ",
-                '"',
-                MXServers,
-                IPAddress,
-                Hostnames,
-                strict,
-                // relayIPAddressOutput,
-                '"'
-            );
-    });
-
-    //auto insert IP relay in answer box
-    // $("#relayIPAddress").keyup(x => {
-    //     var recordInfo = "ip4:";
-    //     var relayIPAddress = $("#relayIPAddress").val();
-    //     if (
-    //         x.originalEvent.key === "Backspace" &&
-    //         relayIPAddress.length === 1
-    //     ) {
-    //         $("#relayIPAddress").attr(
-    //             "placeholder",
-    //             "IP address(es) in CIDR format"
-    //         );
-    //     } else {
-    //         recordInfo.concat(relayIPAddress);
-    //     }
-    //     $(printToAnswer);
-    //     //need to figure out how to get this function to fire on key up to append the value to the end of #domain but inside the quotes...
-    // });
-
-    // function printToAnswer() {
-    //return form inputs
-    // $("#answer")
-    //     .text(yourDomain)
-    //     .append(
-    //         recordInfo,
-    //         " ",
-    //         '"',
-    //         MXServers,
-    //         IPAddress,
-    //         Hostnames,
-    //         strict,
-    //         relayIPAddressOutput,
-    //         '"'
-    //     );
-    // }
+//auto insert domain name in #answer
+let yourDomain = "example.com";
+let recordInfo = ". IN TXT";
+$(domain).keyup(x => {
+    if (x.originalEvent.key === "Backspace" && yourDomain.length === 1) {
+        yourDomain = "example.com";
+    } else {
+        yourDomain = $("#domain").val();
+    }
+    printAnswer();
+    //i need this to also run the getAnswers function when yourDomain value changes
 });
 
-//IP Address
 
-// const answerText = domain.value + "" + 'IN TXT "v=spf1"';
+//    $("#answer").text(yourDomain);
 
-// //populate answer box
-// function submitForm() {
-//     if (domain.value != undefined) {
-//         answer.innerHTML = answerText;
+//MX Servers, IP Address, Hostnames, and Strict inputs
+
+const inputs = 'input[name=MXServers], input[name=IPAddress], input[name=hostnames], input[name=strict]';
+
+//Event Handler for inputs
+$(inputs).on("click", () => {
+    printAnswer();
+
+});
+
+
+// auto insert IP relay in answer box
+let relayIPAddress = $('#relayIPAddress');
+let relayIPAddressVal = $("#relayIPAddress").val();
+let relayInfo = "ip4:";
+// const relayIPAddressAnswer = () => {
+//     if (relayIPAddressVal.length >= 2) {
+//         concat(relayInfo, relayIPAddressVal);
 //     }
-//     // answer.innerHTML = domain.value + "." + " " + "IN TXT";
-// }
+// };
+
+//IP address event handler
+$("#relayIPAddress").keyup(x => {
+
+    if (
+        x.originalEvent.key === "Backspace" &&
+        relayIPAddressVal.length === 1
+    ) {
+        $(relayIPAddress).attr(
+            "placeholder",
+            "IP address(es) in CIDR format"
+        );
+    }
+
+    printAnswer();
+
+});
+
+
+//Print Answer to DOM
+
+const printAnswer = () => {
+
+    //no longer reruns or duplicates value in answer box
+    let MXServers = $("input[name=MXServers]:checked").val();
+    let IPAddress = $("input[name=IPAddress]:checked").val();
+    let Hostnames = $("input[name=hostnames]:checked").val();
+    let strict = $("input[name=strict]:checked").val();
+
+
+    $(answer).text(yourDomain)
+        .append(recordInfo,
+            " ",
+            '"',
+            MXServers, IPAddress, Hostnames, strict,
+            '"');
+};
